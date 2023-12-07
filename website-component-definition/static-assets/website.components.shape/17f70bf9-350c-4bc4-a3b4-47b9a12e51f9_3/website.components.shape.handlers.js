8eb8d5097905f7c84c0f6b9d790e337c43bad5bb
gamenwp <gamenwp@gmail.com>
2023-12-07 12:19:09 +0200

    new download site
    new download site

---
+++ website-component-definition/static-assets/website.components.shape/17f70bf9-350c-4bc4-a3b4-47b9a12e51f9_3/website.components.shape.handlers.js
@@ -0,0 +1,1 @@
+websiteComponents.loader.define("website.components.shape.handlers",[],(()=>(()=>{"use strict";var e,s={};return e=s,Object.defineProperty(e,"__esModule",{value:!0}),e.default=new class{},s})()));
